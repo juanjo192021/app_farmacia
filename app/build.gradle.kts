2017f09a336fdfe0a48f911c0ed76e0127f07378
2goad <anthonyjeanvargasochoa@gmail.com>
2024-10-28 00:57:10 -0500

    Interfaz registrar producto Carlitas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     // define a BOM and its version
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
 
     // define any required OkHttp artifacts without version
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
 
     implementation ("androidx.appcompat:appcompat:1.4.1") // Verifica si hay una versión más reciente
     implementation ("com.google.android.material:material:1.4.0")// Si usas Material Design
